2679917476cf9336217481dced543f6db0904b9c
cchameyr <cchameyr@e2r5p13.42.fr>
2015-12-07 17:20:59 +0100

    clem

--- sources/header.h
+++ sources/header.h
@@ -1,19 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/07 16:26:41 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/12/07 17:20:26 by cchameyr         ###   ########.fr       */
+/*   Created: 2015/12/07 17:07:11 by cchameyr          #+#    #+#             */
+/*   Updated: 2015/12/07 17:10:09 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
-	printf("%s, %d", argv[1], argc);
-	return (0);
-}
