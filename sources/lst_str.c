6e98700aa3c2b86ab7a660f4daf7065b08757304
aperraul <alexp-_-@hotmail.fr>
2015-12-08 00:29:19 +0100

    alex

--- sources/lst_str.c
+++ sources/lst_str.c
@@ -6,9 +6,125 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 17:03:45 by aperraul          #+#    #+#             */
-/*   Updated: 2015/12/07 17:19:34 by aperraul         ###   ########.fr       */
+/*   Updated: 2015/12/08 00:28:36 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "struct_s.h"
-t_list	ft_addstr()
+#include "structs.h"
+#include <stdlib.h>
+
+l_str	*ft_addstr(l_str *begin, char *str)
+{
+	l_str	*list;
+	int		i;
+
+	list = begin;
+	if (!list)
+		list = ft_newlstr(NULL);
+	while (list->next)
+		list = list->next;
+	if (str)
+	{
+		i = 0;
+		while (str[i])
+		{
+			if (list->index < 32)
+			{
+				list->index = str[i];
+				list->index++;
+				i++;
+			}
+			else
+			{
+				list->next = ft_newlstr(list);
+				list = list->next;
+			}
+		}
+	}
+	return (begin);
+}
+
+
+l_str	*ft_addc(l_str *begin, char c) 
+{
+	l_str	*list;
+
+	list = begin;
+	if(!list)
+		list = ft_newlstr(NULL);
+	while (list->next)
+		list = list->next;
+	if (c)
+	{
+		if (list->index < 32)
+		{
+			list->index = c;
+			list->index++;
+		}
+		else
+		{
+			list->next = ft_newlstr(list);
+			list = list->next;
+			list->index = c;
+			list->index++;
+		}
+	}
+	return (begin);
+}
+
+l_str	*ft_newlstr(l_str *begin)
+{
+	l_str	*list;
+
+	list = (l_str *)malloc(sizeof(l_str));
+	if (!begin)
+		begin = list;
+	else
+	{
+		while (begin->next)
+			begin = begin->next;
+		begin->next = list;
+	}
+	list->next = NULL;
+	list->index = 0;
+	ft_bzero(list->str, 32);
+	return (begin);
+}
+
+int		ft_indexcpt(l_str *begin)
+{
+	int		i;
+	l_str	*list;
+
+	if(!begin)
+		return (0);
+	list = begin;
+	i = 0;
+	if (list->next)#
+	{
+		while (list->next)
+		{
+			i += list->index;
+			list = list->next;
+		}
+	}
+	while (list->str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_freelststr(l_str *begin)
+{
+	l_str	*list;
+
+	if (begin)
+	{
+		while (list->next)
+		{
+			free(list);
+			list->next
+		}
+	}
+}
--- sources/structs.h
+++ sources/structs.h
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   struct_s.h                                         :+:      :+:    :+:   */
+/*   structs.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 17:05:29 by aperraul          #+#    #+#             */
-/*   Updated: 2015/12/07 17:10:36 by aperraul         ###   ########.fr       */
+/*   Updated: 2015/12/08 00:13:49 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUC_S_H
-# define STRUC_S_H
+#ifndef STRUCS_H
+# define STRUCS_H
 
-typedef	struct	t_str
+# include "libft.h"
+
+typedef	struct		t_str
 {
-	char	str[32];
-	int		index;
-	t_str	*next;
-}				l_str;
+	char			str[32];
+	int				index;
+	struct t_str	*next;
+}					l_str;
+
+l_str	*ft_addstr(l_str *begin, char *str);
+l_str	*ft_addc(l_str *begin, char c);
+l_str	*ft_newlstr(l_str *begin);
+int		ft_indexcpt(l_str *begin);
 
 #endif
