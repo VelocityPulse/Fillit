215395004cb71293c5d008c578f80a0911ee1644
cchameyr <cchameyr@e2r5p13.42.fr>
2015-12-08 07:40:24 +0100

    clem

--- sources/check_linked.c
+++ sources/check_linked.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 07:21:21 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/12/08 07:21:22 by cchameyr         ###   ########.fr       */
+/*   Updated: 2015/12/08 07:40:01 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		ft_checklinkedfirstline(t_form *form, int x, int y)
 		if (form->shape[y][x + 1] == '#')
 			match++;
 	}
-	if (x > 0)
+	else if (x > 0)
 	{
 		if (form->shape[y + 1][x] == '#')
 			match++;
@@ -48,13 +48,41 @@ int		ft_checklinkedlastline(t_form *form, int x, int y)
 		if (form->shape[y][x + 1] == '#')
 			match++;
 	}
-	if (x > 0)
+	else if (x > 0)
 	{
 		if (form->shape[y - 1][x] == '#')
 			match++;
 		if (form->shape[y][x + 1] == '#')
 			match++;
 		if (form->shape[y][x - 1] == '#')
+			match++;
+	}
+	return (match);
+}
+
+int		ft_checklinkedmiddle(t_form *form, int x, int y)
+{
+	int match;
+
+	match = 0;
+	if (x == 0)
+	{
+		if (form->shape[y - 1][x] == '#')
+			match++;
+		if (form->shape[y + 1][x] == '#')
+			match++;
+		if (form->shape[y][x + 1] == '#')
+			match++;
+	}
+	else if (x > 0)
+	{
+		if (form->shape[y - 1][x] == '#')
+			match++;
+		if (form->shape[y + 1][x] == '#')
+			match++;
+		if (form->shape[y][x - 1] == '#')
+			match++;
+		if (form->shape[y][x + 1] == '#')
 			match++;
 	}
 	return (match);
@@ -67,18 +95,23 @@ int		ft_checklinkedform(t_form *form, int x, int y)
 	match = 0;
 	while (y < 4)
 	{
+		x = 0;
 		while (x < 4)
 		{
-			if (y == 0)
-				match += ft_checklinkedfirstline(form, x, y);
-			else if (y == 3)
-				match += ft_checklinkedlastline(form, x, y);
-			else
+			if (form->shape[y][x] == '#')
 			{
-				if (form->shape[][])
+				if (y == 0)
+					match += ft_checklinkedfirstline(form, x, y);
+				else if (y == 3)
+					match += ft_checklinkedlastline(form, x, y);
+				else
+					match += ft_checklinkedmiddle(form, x, y);
 			}
 			x++;
 		}
 		y++;
 	}
+	if (match == 4 || match == 8)
+		return (1);
+	return (0);
 }
