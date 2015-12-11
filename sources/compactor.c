ff9b0f3770b7ef774307b895bb9239891aa1c331
Alexis PERRAULT <aperraul@e3r6p12.42.fr>
2015-12-11 11:45:02 +0100

    alex

--- sources/compactor.c
+++ sources/compactor.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 03:40:04 by aperraul          #+#    #+#             */
-/*   Updated: 2015/12/08 07:22:48 by cchameyr         ###   ########.fr       */
+/*   Updated: 2015/12/11 11:44:33 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,17 @@ int				ft_nbform(t_form *form)
 	return (list->index);
 }
 
-t_square		*ft_min_size(int size)
+int				ft_min_size(int nbform)
+{
+	int		size;
+
+	size = 2;
+	while (size * size < nbform * 4)
+		size++;
+	return (size);
+}
+
+t_square		*ft_carre_create(int size)
 {
 	int y;
 	t_square	*carre;
@@ -37,18 +47,16 @@ t_square		*ft_min_size(int size)
 	while (y < size)
 	{
 		ft_memset(carre->shape[y], '.' ,size);
-		carre->shape[y][size] = '\n';
 		y++;
 	}
-	carre->shape[size][size] = '\0';
 	return (carre);
 }
 
 t_square		*ft_compact(t_form *form)
 {
-	t_square	carre;
+	t_square	*carre;
 
-	carre = ft_min_size(ft_nbform(form));
+	carre = ft_carre_create(ft_min_size(ft_nbform(form)));
 	while (form->next)
 	{
 		//ckeck de la piece avec les emplacements possible;
